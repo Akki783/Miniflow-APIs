beb9ebf72644a48c66d0d2093853ecf57f832f26
Akki783 <platformsupport@dove-soft.com>
2024-10-10 17:03:02 +0530

    Add files via upload

--- controller/calc.js
+++ controller/calc.js
@@ -1,6 +1,18 @@
 module.exports = (req, res) => {
-    let { travel_1, travel_2, travel_3 } = req.body;
+    try{
+        let { travel_1, travel_2, travel_3 } = req.body;
 
-    let totalCost = [travel_1, travel_2, travel_3].reduce((sum, travel) => sum + (travel === "Yes" ? 5000 : (travel === "No" ? 3000 : 0)), 0);
-    res.send(`TotalCost : ${totalCost}`);
+        let totalCost = [travel_1, travel_2, travel_3].reduce((sum, travel) => sum + (travel === "Yes" ? 5000 : (travel === "No" ? 3000 : 0)), 0);
+        
+        return res.status(400).json({
+            message:"success",
+            totalAmount : totalCost
+        })
+    }
+    catch(error)
+    {
+        return res.status(400).json({
+            message:"Please try again later"
+        })
+    }
 }
