eaeadb6e717189fc02e6b0467adf1b99ebc86794
Akki783 <platformsupport@dove-soft.com>
2024-10-16 13:16:34 +0530

    Add files via upload

--- controller/list.js
+++ controller/list.js
@@ -1,11 +1,11 @@
 module.exports = (req, res) => {
 
     data = {
-        "list 1": "Item 1",
-        "list 2": "Item 2",
-        "list 3": "Item 3",
-        "list 4": "Item 4",
-        "list 5": "Item 5",
+        "list_1": "Item 1",
+        "list_2": "Item 2",
+        "list_3": "Item 3",
+        "list_4": "Item 4",
+        "list_5": "Item 5",
     }
 
     return res.status(400).json({
