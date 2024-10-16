1954e8adc816bea1f96210baea8d50fdc874c286
Akki783 <platformsupport@dove-soft.com>
2024-10-16 15:53:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -23,6 +23,61 @@ app.get('/', (req,res)=>{
     })
 });
 
+
+app.get('/model', (req,res)=>{
+
+    let {model} = req.body;
+
+    model = model.toLowerCase();
+
+    const carModels = {
+        toyota: {
+            corolla: "Corolla",
+            camry: "Camry",
+            rav4: "RAV4",
+            highlander: "Highlander"
+        },
+        honda: {
+            civic: "Civic",
+            accord: "Accord",
+            crv: "CR-V",
+            pilot: "Pilot"
+        },
+        ford: {
+            fiesta: "Fiesta",
+            focus: "Focus",
+            mustang: "Mustang",
+            explorer: "Explorer"
+        },
+        bmw: {
+            series3: "3 Series",
+            series5: "5 Series",
+            x5: "X5",
+            x3: "X3"
+        },
+        mercedes: {
+            cclass: "C-Class",
+            eclass: "E-Class",
+            gla: "GLA",
+            glc: "GLC"
+        },
+        audi: {
+            a3: "A3",
+            a4: "A4",
+            q5: "Q5",
+            q7: "Q7"
+        }
+    };
+    
+    
+    
+    res.status(200).json({
+        success:true,
+        data: carModels.model
+    })
+});
+
+
 app.listen(port, () => {
     console.log(`Server started at localhost:${port}`);
 })
